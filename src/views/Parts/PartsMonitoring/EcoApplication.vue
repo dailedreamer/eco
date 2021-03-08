f2ad65054789f2eee2343c8c847e32cfc0be5013
George Bien Almenanza <georgebien.almenanza@ph.fujitsu.com>
2021-03-08 11:43:41 +0800

    [Parts] Added: Add folder for Parts Monitoring

---
+++ src/views/Parts/PartsMonitoring/EcoApplication.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <Side/>
+  </div>
+</template>
+
+<script>
+import Side from "../../../components/Parts/PartsMonitoring/Side.vue";
+
+export default {
+    name: {
+        Side
+    }
+}
+</script>
+
+<style>
+
+</style>
