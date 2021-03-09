a2daa231d5504881f4eeca4abea99447399af011
Donna Madronio <donna.madronio@ph.fujitsu.com>
2021-03-09 14:37:32 +0800

    [MERGING CONFLICTS]

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -21,12 +21,7 @@ faThumbsUp,
 faHandHoldingUsd,
 faPallet,
 faFlagCheckered,
-<<<<<<< HEAD
-faFileExcel,
-faSave
-=======
 faEdit
->>>>>>> 654e9fa32eb7fd9b240e2a66575aa271e02cd160
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
@@ -50,13 +45,8 @@ library.add(
 	faPallet,
 	faHandHoldingUsd,
 	faFlagCheckered,
-<<<<<<< HEAD
-	faFileExcel,
-	faSave
-=======
 	faEdit
 
->>>>>>> 654e9fa32eb7fd9b240e2a66575aa271e02cd160
 );
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
