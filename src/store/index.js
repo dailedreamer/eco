a2f2911193cd20baa7aa52b7cb81f11600b5c964
Afelo <edralyn.afelo@fujitsu.com>
2021-03-12 09:22:11 +0800

    [MERGING CONFLICTS]

--- src/store/index.js
+++ src/store/index.js
@@ -6,17 +6,14 @@ import auth from "./auth";
 import alpha from "../template/modules/alpha";
 import parts_monitoring from "./modules/Parts/PartsMonitoring/parts_monitoring";
 import email from "./modules/email.js";
-<<<<<<< HEAD
+
 import parts_device from "./modules/management/parts_device.js";
 import parts_model from "./modules/management/parts_model.js";
 import parts_unit from "./modules/management/parts_unit.js";
 
-=======
 import partsRegistrationUnit from "./modules/Parts/PartsRegistrationUnit";
 import partsRegistration from "./modules/Parts/PartsRegistration";
 
-import parts from "./modules/management/parts.js";
->>>>>>> 922bd3c73b72136a446cca2ac1bd8c5e15330e91
 
 export default new Vuex.Store({
   state: {},
@@ -27,15 +24,10 @@ export default new Vuex.Store({
       parts_monitoring,
      email, 
      auth, 
-<<<<<<< HEAD
      parts_device,
      parts_model,
-     parts_unit
-
-=======
-     parts,
+     parts_unit,
      partsRegistrationUnit,
      partsRegistration
->>>>>>> 922bd3c73b72136a446cca2ac1bd8c5e15330e91
   }
 });
