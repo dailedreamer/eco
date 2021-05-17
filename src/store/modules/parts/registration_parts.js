b99299430d345364bc23eb3a7709711381405dfe
Afelo <edralyn.afelo@fujitsu.com>
2021-05-17 13:40:38 +0800

    [PARTS REGISTRATION] Added: #6242 Parts PE Multiple Registration - Plotting File Upload

--- src/store/modules/parts/registration_parts.js
+++ src/store/modules/parts/registration_parts.js
@@ -1,35 +1,32 @@
+import axios from "axios";
+
 // import axios from "axios";
 export default {
   state: {
     parts_registration_units:[],
   },
   mutations: {
-    SET_PARTS_REGISTRATION_UNITS(state, units) {
-        state.units = units;
-      },
+
   },
   actions: {
-      // async loadPartsRegistrationUnit({ commit }) {
-      //   return new Promise((resolve, reject) => {
-      //     // axios
-      //     //   .get("devices")
-      //     //   .then(function(response) {
-      //     //     commit("SET_PARTS_REGISTRATION_UNITS", response.data);
-      //     //     let result = {
-      //     //       code: response.data.code,
-      //     //       status: response.data.status,
-      //     //       message: response.data.message,
-      //     //       data: response.data.data,
-      //     //     };
-      //     //     resolve(result);
-      //     //   })
-      //     //   .catch(function(error) {
-      //     //     reject(error);
-      //     //   });
-      //   });
-      //   },
+      async uploadPartsRegistrationParts(state, payload){
+        return new Promise((resolve, reject)=>{
+          axios
+            .post(`parts-pe/file-upload/${payload[1]}`, payload[0], {
+              header: {
+                  "Content-Type": "multipart/form-data",
+              },
+            })
+            .then(function(response) {
+                resolve(response);
+            })
+            .catch(function(error){
+                reject(error);
+            });
+        }) 
+      }
   },
   getters: {
-         getPartsRegistrationUnits: (state) => state.parts_registration_units,
+    
   }
 }
--- src/views/Parts/PartsRegistrationParts.vue
+++ src/views/Parts/PartsRegistrationParts.vue
@@ -193,6 +193,13 @@
           </b-col>
           <b-col cols="9">
             <b-card class="card_style">
+              <b-row>
+                  <b-col cols="5">
+                  <h4>MULTIPLE ITEM REGISTRATION</h4>
+                  <hr class="hr--property" />
+                  <div class="red-line"></div>
+                  </b-col>
+              </b-row>
               <b-media class="pb-2 pt-1">
                 <template #aside>
                   <b-img 
@@ -213,13 +220,13 @@
                 <b-col>
                   <b-row>
                     <b-col cols="3">
-                      <b-form-file 
-                        id="input-file"
-                        name="file"
-                        type="file"
-                        accept=".csv" 
-                        v-on:change="FileUpload()">
-                      </b-form-file>
+                      <input
+                          class="alpha-input"
+                          id="input-file"
+                          name="file"
+                          type="file"
+                          required
+                        />
                     </b-col>
                     <b-col cols="3">
                       <b-button
@@ -227,20 +234,31 @@
                         id="button-upload"
                         type="submit"
                         title="Click to Upload"
-                        variant="danger"
-                        size="sm">
+                        variant="danger">
                         <font-awesome-icon 
                           icon="upload" 
                           size="sm" 
                           class="icon"/>Upload
                       </b-button>
+                      <b-button
+                        class="mt-1 ml-2"
+                        id="button-clear"
+                        variant="outline-secondary"
+                        title="Click to Clear Inputs"
+                        @click="clearUpload()">
+                        <font-awesome-icon 
+                          icon="times-circle" 
+                          size="sm" 
+                          class="icon"/> 
+                          Clear
+                      </b-button>
                     </b-col>
                       <b-col cols="3" class="mt-2">
                         <label>Device: </label>     
-                        <label id="lbl_device"></label>
+                        <label id="lbl_device">{{items.device_name}}</label>
                       </b-col>
                       <b-col cols="3" class="mt-2">
-                        <label id="lbl_model"></label>
+                        <label id="lbl_model">{{items.name}}</label>
                         <label>Model: </label>
                       </b-col>
                     </b-row>
@@ -359,11 +377,11 @@ export default {
         ],
         items: 
         [
-          {id: 1, batch_number: '123456', parent_drawing_number: 'KD111-123456', drawing_number_current_revision: '01', part_number: 'KD001-111111', part_number_current_revision: '01', class: 'A', die_details: 'sample' },
-          {id: 2, batch_number: '234567', parent_drawing_number: 'KD222-123456', drawing_number_current_revision: '04', part_number: 'KD001-121212', part_number_current_revision: '05', class: 'B', die_details: 'sample' },
-          {id: 3, batch_number: '234568', parent_drawing_number: 'KD333-123456', drawing_number_current_revision: '01', part_number: 'KD001-232323', part_number_current_revision: '02', class: 'C', die_details: 'sample' },
-          {id: 4, batch_number: '234569', parent_drawing_number: 'KD444-123456', drawing_number_current_revision: '02', part_number: 'KD001-343434', part_number_current_revision: '03', class: 'D', die_details: 'sample' },
-          {id: 5, batch_number: '234570', parent_drawing_number: 'KD555-123456', drawing_number_current_revision: '05', part_number: 'KD001-454545', part_number_current_revision: '06', class: 'E', die_details: 'sample' },
+          // {id: 1, batch_number: '123456', parent_drawing_number: 'KD111-123456', drawing_number_current_revision: '01', part_number: 'KD001-111111', part_number_current_revision: '01', class: 'A', die_details: 'sample' },
+          // {id: 2, batch_number: '234567', parent_drawing_number: 'KD222-123456', drawing_number_current_revision: '04', part_number: 'KD001-121212', part_number_current_revision: '05', class: 'B', die_details: 'sample' },
+          // {id: 3, batch_number: '234568', parent_drawing_number: 'KD333-123456', drawing_number_current_revision: '01', part_number: 'KD001-232323', part_number_current_revision: '02', class: 'C', die_details: 'sample' },
+          // {id: 4, batch_number: '234569', parent_drawing_number: 'KD444-123456', drawing_number_current_revision: '02', part_number: 'KD001-343434', part_number_current_revision: '03', class: 'D', die_details: 'sample' },
+          // {id: 5, batch_number: '234570', parent_drawing_number: 'KD555-123456', drawing_number_current_revision: '05', part_number: 'KD001-454545', part_number_current_revision: '06', class: 'E', die_details: 'sample' },
         ]
       }
     },
@@ -383,24 +401,41 @@ export default {
     {
         uploadFile: function()
         {
-          document.getElementById("button-upload").disabled = true;
-            let formData = new FormData(document.getElementById("form-upload")); 
-            
-            this.$store.dispatch("uploadExcel",formData)
-            .then((response) => {
-                console.log(response)
-            });
-        },
-        submitForm: function()
-        {
-            var formData = new FormData(document.getElementById('form-parts-registration'));
-            document.getElementById('button-submit').disabled.true;
+          var formData = new FormData();
+          var excelFile = document.querySelector("#input-file");
+          let fileType = excelFile.files[0].name.split('.')[1];
+          var token = "7MegNJKG3LgpU5rMxzGCKgyCm1nicLI7SnI1Q1HzWcviSTi6tyBfh79ereFrgOfr";
+        
+          if((fileType !== 'csv') && (fileType !== 'xlsx')){
+            document.getElementById("input-file").value = "";
+            this.toast("Warning", "Please Upload a CSV or XLSX file type only.");
+          }
+          else{
+            formData.append("file_name", excelFile.files[0]);
+
             this.$store
-            .dispatch("insertPartsRegistrationPe", formData)
-            .then((response) => {
-                console.log(JSON.stringify(this.form));
-                console.log(response)
-            });
+            .dispatch("uploadPartsRegistrationParts", [formData, token])
+            .then((response)=>{
+              this.items = response.data.result.inserted;
+              let status = response.data.status;
+              if(status == "Success")
+              {
+                document.getElementById("input-file").value = "";
+                this.toast(status, response.data.message);
+              }
+              else if(status == "Warning")
+                this.toast(status, response.data.message);
+              else
+                this.toast(status, response.data.message);
+              })
+              .catch((error) => {
+                this.toast("error", "Something went wrong");
+                document.getElementById("input-file").value = "";
+                console.log(error);
+              })
+              .finally(() => {
+              });
+          }
         },
         loadDevice: function()
         {
@@ -409,7 +444,7 @@ export default {
               // let data = response.data.data;
               // this.deviceOptions = data;   
               // console.log(this.deviceOptions); 
-              console.log(response);
+              // console.log(response);
               let information = response.data.data;
               Object.keys(information).forEach((key) => {
                   this.deviceOptions.push({
@@ -489,6 +524,15 @@ export default {
               validation:"",
             },
         };
+    },
+    toast: function (status, message){
+        this.$toast(message, {
+            type:status.toLowerCase().trim(),
+            position: "bottom-right",
+        });
+    },
+    clearUpload: function(){
+        document.getElementById("input-file").value = "";
     },
 }
 }
